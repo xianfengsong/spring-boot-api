b0f0261919e3f85b6e63efc417502f70b26fdea6
xianfengsong <xianfeng635@gmail.com>
2019-09-12 20:51:24 +0800

    B树

---
+++ src/main/java/btree/BplusTree.java
@@ -0,0 +1,15 @@
+package btree;
+
+/**
+ * author Xianfeng <br/>
+ * date 19-9-11 下午8:43 <br/>
+ * Desc:
+ */
+public class BplusTree {
+
+    class Node {
+
+        Node left, right;
+        int value;
+    }
+}
--- src/main/java/question/easy/array/Solution.java
+++ src/main/java/question/easy/array/Solution.java
@@ -1,14 +1,18 @@
 package question.easy.array;
 
+import java.util.Arrays;
+
 public class Solution {
+
     /**
      * 从排序数组中删除重复项,在原地删除重复出现的元素
      * 不要使用额外的数组空间
+     *
      * @param nums 排序数组
      * @return 数组的新长度
      */
     public int removeDuplicates(int[] nums) {
-        if(nums==null||nums.length==0){
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         //这个简洁多了。。。
@@ -20,25 +24,38 @@ public class Solution {
 //            }
 //        }
 //        return index+1;
-        int newLength=1;
-        boolean moved=false;
+        int newLength = 1;
+        boolean moved = false;
 
-        for (int pointer=0;pointer<nums.length-1;pointer++){
+        for (int pointer = 0; pointer < nums.length - 1; pointer++) {
 
-            int compare=nums[pointer];
-            for(int i=pointer+1;i<nums.length;i++){
+            int compare = nums[pointer];
+            for (int i = pointer + 1; i < nums.length; i++) {
                 if (compare != nums[i]) {
                     newLength++;
-                    if(moved){
-                        nums[newLength-1]=nums[i];
-                        pointer=i-1;
+                    if (moved) {
+                        nums[newLength - 1] = nums[i];
+                        pointer = i - 1;
                     }
                     break;
-                }else {
-                    moved=true;
+                } else {
+                    moved = true;
                 }
             }
         }
         return newLength;
+    }
+
+    //是否重复
+    public boolean isRepeat(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+
+        }
+        return false;
     }
 }
