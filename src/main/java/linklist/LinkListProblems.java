677f013cb391863ee472cc2783787e1005001b88
xianfengsong <xianfeng635@gmail.com>
2019-11-05 00:41:17 +0800

    linklist

---
+++ src/main/java/linklist/LinkListProblems.java
@@ -0,0 +1,22 @@
+package linklist;
+
+public class LinkListProblems {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 反转一个单链表。
+     * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+     * @param head h
+     * @return n
+     */
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+}
