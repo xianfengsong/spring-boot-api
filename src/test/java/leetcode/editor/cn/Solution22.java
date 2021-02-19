a3a8944010f5a20f6f02e79e612b5421df355637
songxianfeng <songxianfeng@xiaomi.com>
2021-02-19 20:20:38 +0800

    no.22

---
+++ src/test/java/leetcode/editor/cn/Solution22.java
@@ -0,0 +1,59 @@
+package leetcode.editor.cn;
+//数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 3
+//输出：["((()))","(()())","(())()","()(())","()()()"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：["()"]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 8 
+// 
+// Related Topics 字符串 回溯算法 
+// 👍 1565 👎 0
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution22 {
+    public List<String> generateParenthesis(int n) {
+        return foo(n,n,"");
+    }
+    private List<String> foo(int l,int r,String s){
+        List<String> ans = new ArrayList<>();
+        if(l==0&&r==0){
+            ans.add(s);
+        }else{
+            if(l>0){
+                ans.addAll(foo(l-1,r,s+"("));
+            }
+            if(l<r&&r>0){
+                ans.addAll(foo(l,r-1,s+")"));
+            }
+        }
+        return ans;
+    }
+    public static void main(String []args){
+        System.out.println(new Solution22().generateParenthesis(3));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
