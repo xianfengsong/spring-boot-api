e390f1553c13baaf754a9adf750260ee15b4a9f3
xianfengsong <xianfeng635@gmail.com>
2021-03-09 20:08:39 +0800

    lc.132 实现题解，使用dp

--- src/test/java/leetcode/editor/cn/Solution132.java
+++ src/test/java/leetcode/editor/cn/Solution132.java
@@ -43,11 +43,63 @@ package leetcode.editor.cn;
 // 👍 348 👎 0
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution132 {
-    private int part = 0;
 
     public int minCut(String s) {
+        Set<String> splits = new HashSet<>();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                checkSplits(arr, i, j, splits);
+            }
+        }
+        //00 01 11 22
+        int[] dp = new int[s.length()];
+        dp[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            int min = s.length();
+            if (splits.contains(0 + "," + i)) {
+                dp[i] = 0;
+            } else {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (splits.contains((j + 1) + "," + i)) {
+                        dp[i] = Math.min(dp[j] + 1, min);
+                    }
+                }
+            }
+        }
+        return dp[s.length() - 1];
+    }
+
+    public boolean checkSplits(char[] arr, int i, int j, Set<String> splits) {
+        String key = i + "," + j;
+        if (splits.contains(key)) {
+            return true;
+        }
+        if (i < j) {
+            if (arr[i] == arr[j] && checkSplits(arr, i + 1, j - 1, splits)) {
+                splits.add(key);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int part = 0;
+
+    /**
+     * 思路：
+     * 按回文串长度从大到小枚举出s中的回文子串ans，然后计算要得到ans需要的分割次数n
+     * ans只保存最长的回文子串，所以n最小
+     * 问题：有个case不过，没发现哪里有问题
+     */
+    public int minCutFail(String s) {
         int len = s.length();
         if (len > 0) {
             String ans = clean(s, len);
@@ -62,10 +114,10 @@ class Solution132 {
                 int ed = st + ans.length();
 
                 if (st != 0) {
-                    minCut(s.substring(0, st));
+                    minCutFail(s.substring(0, st));
                 }
                 if (ed <= s.length() - 1) {
-                    minCut(s.substring(ed));
+                    minCutFail(s.substring(ed));
                 }
             }
         }
@@ -98,7 +150,8 @@ class Solution132 {
 
     public static void main(String[] args) {
         Solution132 s = new Solution132();
-        System.out.println("452="+s.minCut("apjesgpsxoeiokmqmfgvjslcjukbqxpsobyhjpbgdfruqdkeiszrlmtwgfxyfostpqczidfljwfbbrflkgdvtytbgqalguewnhvvmcgxboycffopmtmhtfizxkmeftcucxpobxmelmjtuzigsxnncxpaibgpuijwhankxbplpyejxmrrjgeoevqozwdtgospohznkoyzocjlracchjqnggbfeebmuvbicbvmpuleywrpzwsihivnrwtxcukwplgtobhgxukwrdlszfaiqxwjvrgxnsveedxseeyeykarqnjrtlaliyudpacctzizcftjlunlgnfwcqqxcqikocqffsjyurzwysfjmswvhbrmshjuzsgpwyubtfbnwajuvrfhlccvfwhxfqthkcwhatktymgxostjlztwdxritygbrbibdgkezvzajizxasjnrcjwzdfvdnwwqeyumkamhzoqhnqjfzwzbixclcxqrtniznemxeahfozp"));
+        System.out.println("452=" + s
+                .minCut("apjesgpsxoeiokmqmfgvjslcjukbqxpsobyhjpbgdfruqdkeiszrlmtwgfxyfostpqczidfljwfbbrflkgdvtytbgqalguewnhvvmcgxboycffopmtmhtfizxkmeftcucxpobxmelmjtuzigsxnncxpaibgpuijwhankxbplpyejxmrrjgeoevqozwdtgospohznkoyzocjlracchjqnggbfeebmuvbicbvmpuleywrpzwsihivnrwtxcukwplgtobhgxukwrdlszfaiqxwjvrgxnsveedxseeyeykarqnjrtlaliyudpacctzizcftjlunlgnfwcqqxcqikocqffsjyurzwysfjmswvhbrmshjuzsgpwyubtfbnwajuvrfhlccvfwhxfqthkcwhatktymgxostjlztwdxritygbrbibdgkezvzajizxasjnrcjwzdfvdnwwqeyumkamhzoqhnqjfzwzbixclcxqrtniznemxeahfozp"));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
